33b84407bf781e7c0f54adf49a69489e4a7f37cb
steve.goodwin <stevegoodwin1972@gmail.com>
2026-04-04 17:24:55 +0100

    Archive inactive src modules

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,17 +1,7 @@
-import * as React from "react";
-
-
-// src/utils/types/index.ts
-// Central export point for all type definitions
-
-export * from './api';
-export * from './chat';
-export * from './common';
-export * from './product';
-export * from './project';
-export * from './proposal';
-export * from './training';
-export * from './user';
-
-
-
+// Central export point for active shared type definitions.
+export * from "./api";
+export * from "./common";
+export * from "./product";
+export * from "./project";
+export * from "./proposal";
+export * from "./user";
