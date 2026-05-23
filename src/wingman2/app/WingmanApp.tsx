ee301481c5689c43bc1a8dbb163543db928b5f34
SteveGoodwinApps <stevegoodwin1972@gmail.com>
2026-05-23 08:16:49 +0100

    Wire call card voice and framing installers

--- src/wingman2/app/WingmanApp.tsx
+++ src/wingman2/app/WingmanApp.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { Navigate, useRoutes } from "react-router-dom";
+import { installCallCardsAudienceFraming } from "../utils/installCallCardsAudienceFraming";
+import { installCallCardsVoiceCapture } from "../utils/installCallCardsVoiceCapture";
 import { wingmanRoutes } from "./routes";
 
 export default function WingmanApp() {
+  useEffect(() => {
+    installCallCardsAudienceFraming();
+    installCallCardsVoiceCapture();
+  }, []);
+
   return useRoutes([
     ...wingmanRoutes,
     { path: "*", element: <Navigate to="/wingman/dashboard" replace /> },
