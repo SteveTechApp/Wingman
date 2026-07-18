53d66ceeb7e59ad1723e485573f21b671433e9c1
SteveGoodwinApps <stevegoodwin1972@gmail.com>
2026-07-18 18:23:27 +0100

    Load approved reference alignment after visual polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./wingman2/styles/wingman-style-stack.css";
 import "./wingman2/styles/wingman-reference-theme.css";
 import "./wingman2/styles/wingman-workflow-theme.css";
 import "./wingman2/styles/wingman-visual-polish.css";
+import "./wingman2/styles/wingman-approved-reference-alignment.css";
 import { installCompareManufacturerAssist } from "./wingman2/lib/compareManufacturerAssist";
 import "./wingman2/lib/guruDetachedPanel";
 import "./wingman2/lib/microphoneSafety";
